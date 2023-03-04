4fda46d900ec0e7ad95516d47b82e2cc5a5faa6d
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2023-03-04 14:08:58 -0300

    feat: implementando broken authenticator

---
+++ clj-owasp/src/clj_owasp/broken_authenticator.clj
@@ -0,0 +1,29 @@
+(ns clj_owasp.broken_authenticator
+  (:require [crypto.password.bcrypt :as password]))
+
+(def database (atom {}))
+
+(defn add [table document]
+  (swap! database update-in [table] conj document))
+
+(defn register-new-user! [username password]
+  (add :users {:username username, :password password}))
+
+;aqui o problema eh que a senha esta armazenada em texto puro
+;tem que criptografar!
+(println (register-new-user! "arthur.tatay" "banana"))
+
+
+;solucao: podemos utilizar uma lib do clojure para criptografia
+;criar uma funcao, e sempre criptografar a senha antes de salvar no banco
+(defn encrypted-password [password]
+  (password/encrypt password))
+
+(defn register-new-user! [username password]
+  (add :users {:username username, :password (encrypted-password password)}))
+
+(println (register-new-user! "arthur.tatay" "banana"))
+
+
+;validando a criptografia
+(println (password/check "banana" "$2a$11$iUGuc0TEJG/RaGadwnxle.zo30SXBph1CIhJJ0p.g99ufiBHaWYo6"))
